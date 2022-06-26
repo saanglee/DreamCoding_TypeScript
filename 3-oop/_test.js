6b2368e221a340b51796cbf51069356d79ad7d7f
sangji <sangllee20@gmail.com>
2022-06-26 18:15:47 +0900

    Chore

--- 3-oop/_test.js
+++ 3-oop/_test.js
@@ -1,125 +1,41 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 {
-    // 부모 클래스
-    var CoffeeMachine_1 = /** @class */ (function () {
-        function CoffeeMachine(beans) {
-            this.coffeeBeans = 0; // instance (object) level
-            this.coffeeBeans = beans;
+    var StackImpl = /** @class */ (function () {
+        function StackImpl(capacity) {
+            this.capacity = capacity;
+            this._size = 0;
         }
-        // 인스턴스 생성 함수
-        CoffeeMachine.makeMachine = function (beans) {
-            return new CoffeeMachine(beans);
-        };
-        // 커피콩 채우는 함수
-        CoffeeMachine.prototype.fillCoffeeBeans = function (beans) {
-            if (beans < 0) {
-                throw new Error("value for beans should be greater than 0");
+        Object.defineProperty(StackImpl.prototype, "size", {
+            get: function () {
+                return this._size;
+            },
+            enumerable: false,
+            configurable: true
+        });
+        StackImpl.prototype.push = function (value) {
+            if (this.size === this.capacity) {
+                throw new Error("Stack is full!");
             }
-            this.coffeeBeans += beans;
+            var node = { value: value, next: this.head };
+            this.head = node;
+            this._size++;
         };
-        // 청소 함수
-        CoffeeMachine.prototype.clean = function () {
-            console.log("cleaning the machine...🧼");
-        };
-        // 커피콩 가는 함수
-        CoffeeMachine.prototype.grindBeans = function (shots) {
-            if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
-                throw new Error("Not enough coffee beans~!⚠️");
+        StackImpl.prototype.pop = function () {
+            if (this.head == null) {
+                throw new Error("Stack is empty!");
             }
-            console.log("grinding beans for ".concat(shots));
-            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+            var node = this.head;
+            this.head = node.next;
+            this._size--;
+            return node.value;
         };
-        // 예열 함수
-        CoffeeMachine.prototype.preheat = function () {
-            console.log("heating up... 🔥");
-        };
-        // 에스프레소 추출 함수
-        CoffeeMachine.prototype.extract = function (shots) {
-            console.log("Pulling ".concat(shots, " shots... \u2615\uFE0F"));
-            return {
-                shots: shots,
-                hasMilk: false
-            };
-        };
-        // 커피 만드는 함수!
-        // (커피 갈고, 기계 예열하는 함수 호출 & 에스프레소 추출 함수  리턴)
-        CoffeeMachine.prototype.makeCoffee = function (shots) {
-            this.grindBeans(shots);
-            this.preheat();
-            return this.extract(shots);
-        };
-        CoffeeMachine.BEANS_GRAMM_PER_SHOT = 7; // class level
-        return CoffeeMachine;
+        return StackImpl;
     }());
-    // CoffeeMachine 상속받는 라떼머신 클래스
-    var CaffeeLatteMachine = /** @class */ (function (_super) {
-        __extends(CaffeeLatteMachine, _super);
-        function CaffeeLatteMachine(beans, serialNumber) {
-            var _this = 
-            // Constructors for derived classes must contain a 'super' call
-            _super.call(this, beans) || this;
-            _this.serialNumber = serialNumber;
-            return _this;
-        }
-        CaffeeLatteMachine.prototype.steamMilk = function () {
-            console.log("Steaming some milk... 🥛");
-        };
-        CaffeeLatteMachine.prototype.makeCoffee = function (shots) {
-            var coffee = _super.prototype.makeCoffee.call(this, shots);
-            // 부모 클래스의 함수에 접근 시 super키워드를 사용한다.
-            this.steamMilk();
-            return __assign(__assign({}, coffee), { hasMilk: true });
-        };
-        return CaffeeLatteMachine;
-    }(CoffeeMachine_1));
-    // offeeMachine 상속받는 설탕 추가 클래스
-    var SweetCoffeeMachine = /** @class */ (function (_super) {
-        __extends(SweetCoffeeMachine, _super);
-        function SweetCoffeeMachine() {
-            return _super !== null && _super.apply(this, arguments) || this;
-        }
-        SweetCoffeeMachine.prototype.makeCoffee = function (shots) {
-            var coffee = _super.prototype.makeCoffee.call(this, shots);
-            console.log("sugar🍬");
-            return __assign(__assign({}, coffee), { hasSugar: true });
-        };
-        return SweetCoffeeMachine;
-    }(CoffeeMachine_1));
-    var machines = [
-        new CoffeeMachine_1(23),
-        new CaffeeLatteMachine(20, "sss"),
-        new SweetCoffeeMachine(11),
-        new CoffeeMachine_1(23),
-        new CaffeeLatteMachine(20, "sss"),
-        new SweetCoffeeMachine(11),
-    ];
-    machines.forEach(function (machine) {
-        console.log("-----------------------------");
-        machine.makeCoffee(1);
-    });
+    var stack = new StackImpl(10);
+    stack.push("Ellie 1");
+    stack.push("Bob 2");
+    stack.push("Steve 3");
+    while (stack.size !== 0) {
+        console.log(stack.pop());
+    }
+    stack.pop();
 }
--- 3-oop/_test.ts
+++ 3-oop/_test.ts
@@ -1,114 +1,49 @@
 {
-  type CoffeeCup = {
-    shots: number;
-    // optional
-    hasMilk?: boolean;
-    hasSugar?: boolean;
+  interface Stack {
+    readonly size: number;
+    push(value: string): void;
+    pop(): string;
+  }
+
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
   };
 
-  interface CoffeeMaker {
-    // 함수 정의
-    makeCoffee(shots: number): CoffeeCup;
-  }
+  class StackImpl implements Stack {
+    private _size: number = 0;
+    private head?: StackNode;
 
-  // 부모 클래스
-  class CoffeeMachine implements CoffeeMaker {
-    private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
-    private coffeeBeans: number = 0; // instance (object) level
-
-    constructor(beans: number) {
-      this.coffeeBeans = beans;
+    constructor(private capacity: number) {}
+    get size() {
+      return this._size;
     }
-
-    // 인스턴스 생성 함수
-    static makeMachine(beans: number): CoffeeMachine {
-      return new CoffeeMachine(beans);
-    }
-    // 커피콩 채우는 함수
-    fillCoffeeBeans(beans: number) {
-      if (beans < 0) {
-        throw new Error("value for beans should be greater than 0");
+    push(value: string) {
+      if (this.size === this.capacity) {
+        throw new Error("Stack is full!");
       }
-      this.coffeeBeans += beans;
+      const node: StackNode = { value, next: this.head };
+      this.head = node;
+      this._size++;
     }
-    // 청소 함수
-    clean(): void {
-      console.log("cleaning the machine...🧼");
-    }
-    // 커피콩 가는 함수
-    private grindBeans(shots: number) {
-      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
-        throw new Error("Not enough coffee beans~!⚠️");
+    pop(): string {
+      if (this.head == null) {
+        throw new Error("Stack is empty!");
       }
-      console.log(`grinding beans for ${shots}`);
-      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
-    }
-    // 예열 함수
-    private preheat(): void {
-      console.log("heating up... 🔥");
-    }
-    // 에스프레소 추출 함수
-    private extract(shots: number): CoffeeCup {
-      console.log(`Pulling ${shots} shots... ☕️`);
-      return {
-        shots,
-        hasMilk: false,
-      };
-    }
-    // 커피 만드는 함수!
-    // (커피 갈고, 기계 예열하는 함수 호출 & 에스프레소 추출 함수  리턴)
-    makeCoffee(shots: number): CoffeeCup {
-      this.grindBeans(shots);
-      this.preheat();
-      return this.extract(shots);
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
     }
   }
 
-  // CoffeeMachine 상속받는 라떼머신 클래스
-  class CaffeeLatteMachine extends CoffeeMachine {
-    constructor(beans: number, public readonly serialNumber: string) {
-      // Constructors for derived classes must contain a 'super' call
-      super(beans);
-    }
-
-    private steamMilk(): void {
-      console.log("Steaming some milk... 🥛");
-    }
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(shots);
-      // 부모 클래스의 함수에 접근 시 super키워드를 사용한다.
-      this.steamMilk();
-
-      return {
-        ...coffee, // 부모 클래스 함수 호출한 리턴값들
-        hasMilk: true,
-      };
-    }
-  }
-  // offeeMachine 상속받는 설탕 추가 클래스
-  class SweetCoffeeMachine extends CoffeeMachine {
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(shots);
-      console.log("sugar🍬");
-
-      return {
-        ...coffee,
-        hasSugar: true,
-      };
-    }
+  const stack = new StackImpl(10);
+  stack.push("Ellie 1");
+  stack.push("Bob 2");
+  stack.push("Steve 3");
+  while (stack.size !== 0) {
+    console.log(stack.pop());
   }
 
-  const machines: CoffeeMaker[] = [
-    new CoffeeMachine(23),
-    new CaffeeLatteMachine(20, "sss"),
-    new SweetCoffeeMachine(11),
-    new CoffeeMachine(23),
-    new CaffeeLatteMachine(20, "sss"),
-    new SweetCoffeeMachine(11),
-  ];
-
-  machines.forEach((machine) => {
-    console.log("-----------------------------");
-    machine.makeCoffee(1);
-  });
+  stack.pop(); // ERROR
 }
